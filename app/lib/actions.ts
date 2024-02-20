7d17a7ce77e0a0b3d79e383e5fa82d79805f99a4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-20 15:46:50 -0500

    Add validation to form data using zod schema

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,10 +6,14 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
-  id: z.string(),
+  id: z.string({ invalid_type_error: 'Please select a customer.' }),
   customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
